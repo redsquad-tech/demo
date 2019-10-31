c3c662093c6c7f6f756955039b61baa06b72fad6
bavadim@gmail.com <bavadim@gmail.com>
2019-10-31 12:21:26 +0300

    add build

---
+++ build/precache-manifest.d25b191bfccd0a4f9221dbd573effd90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b736dd61ed76d4aaeec4df3b43de7705",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a85560ab50616555f2fe",
+    "url": "/static/css/main.693a9b5f.chunk.css"
+  },
+  {
+    "revision": "ae018fcbafcc73b01e8a",
+    "url": "/static/js/2.90933df5.chunk.js"
+  },
+  {
+    "revision": "a85560ab50616555f2fe",
+    "url": "/static/js/main.972769e3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "13c4e31c1ed8ad953b9cb529c0d5a1ea",
+    "url": "/static/media/DeepPavlov.13c4e31c.png"
+  },
+  {
+    "revision": "872931c9476691229ef49e18ba929c68",
+    "url": "/static/media/logo.872931c9.png"
+  }
+]);
